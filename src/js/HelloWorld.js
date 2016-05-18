d4fdd3065d92f5908c9c649b904c21b18b7467b9
Aaron <akr.git.receiver@gmail.com>
2016-05-18 00:14:27 -0400

    completed project added style and font

--- src/js/HelloWorld.js
+++ src/js/HelloWorld.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-
+//exporting the default HelloWorld class that extends the module React Component object
 export default class HelloWorld extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      title : "React Love"
-    }
-  }
+  //rendering function returning our HTML object containing a quote
   render(){
-    return(
-      <h1>I am merely a grain of sand hurtling through space with only self-preceived meaning.</h1>
-    );
+
+    return( <span>I AM MERELY A GRAIN OF SAND HURTLING THROUGH SPACE SEARCHING FOR SIGNICANCE.</span>)
   }
 }
